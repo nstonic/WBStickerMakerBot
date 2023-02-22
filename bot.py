70a52abecf64e3b85217e94051c8a5a0c996a0c9
nstonic <nstonic@gmail.com>
2023-02-22 17:58:14 +0400

    Add number of supplies

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+from collections import Counter
 from pprint import pprint
 
 import telebot
@@ -32,11 +33,9 @@ def start(message: Message):
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'show_supplies')
-def show_supplies(call: CallbackQuery):
+def show_active_supplies(call: CallbackQuery):
     """
     Отображает текущие незакрытые поставки
-    :param call:
-    :return: None
     """
     if response := get_supplies_response(os.environ['WB_API_KEY']):
         supplies = [
@@ -59,7 +58,7 @@ def show_supplies(call: CallbackQuery):
             )
     supplies_markup.add(
         InlineKeyboardButton(
-            text='Показать предыдущие закрытые',
+            text='Показать больше поставок',
             callback_data=f'more_supplies'
         )
     )
@@ -75,12 +74,11 @@ def show_supplies(call: CallbackQuery):
 def show_orders(call: CallbackQuery):
     """
     Отображает заказы из текущей поставки
-    :param call:
-    :return: None
     """
+    supply_id = call.data.lstrip('supply_')
     response = get_orders_response(
         api_key=os.environ['WB_API_KEY'],
-        supply_id=call.data.lstrip('supply_')
+        supply_id=supply_id
     )
     bot.answer_callback_query(call.id, 'Заказы')
 
@@ -88,21 +86,108 @@ def show_orders(call: CallbackQuery):
         Order.parse_obj(order)
         for order in response.json()['orders']
     ]
+    order_markup = InlineKeyboardMarkup()
+    order_markup.add(
+        InlineKeyboardButton(
+            text='Создать стикеры',
+            callback_data=f'stickers_for_supply_{supply_id}'
+        )
+    )
     bot.send_message(
         chat_id=call.message.chat.id,
         text=compile_orders_to_one_message(orders),
+        reply_markup=order_markup
+    )
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('more_supplies'))
+def get_supplies_number(call: CallbackQuery):
+    """
+    Запрашивает количество требуемых поставок
+    """
+    bot.clear_reply_handlers(call.message)
+    bot.register_next_step_handler(
+        call.message,
+        show_number_of_supplies,
+        call=call
+    )
+    bot.send_message(
+        chat_id=call.message.chat.id,
+        text='Сколько последних поставок вы хотите посмотреть? (максимум 50)'
+    )
+
+
+def show_number_of_supplies(message: Message, call: CallbackQuery):
+    """
+    Отображает требуемое число последних поставок
+    """
+    if response := get_supplies_response(os.environ['WB_API_KEY']):
+        supplies = sorted(
+            [
+                Supply.parse_obj(supply)
+                for supply in response.json()["supplies"]
+            ],
+            key=lambda supply: supply.create_at
+        )[::-1]
+    else:
+        try:
+            bot.answer_callback_query(call.id, 'Сервер недоступен. Попробуйте позже')
+        except telebot.apihelper.ApiTelegramException:
+            pass
+        return
+
+    try:
+        supplies_subset = supplies[:int(message.text)][::-1]
+    except ValueError:
+        bot.clear_reply_handlers(call.message)
+        bot.register_next_step_handler(
+            call.message,
+            show_number_of_supplies,
+            call=call
+        )
+        bot.send_message(
+            chat_id=call.message.chat.id,
+            text='Не понял Вас. Введите ещё раз'
+        )
+        return
+
+    is_active = {0: 'Открыта', 1: 'Закрыта'}
+    supplies_markup = InlineKeyboardMarkup()
+    for supply in supplies_subset[:50]:
+        supplies_markup.add(
+            InlineKeyboardButton(
+                text=f'{supply.name} | (создана: {supply.create_at.strftime("%d.%m.%Y")}) | {is_active[supply.done]}',
+                callback_data=f'supply_{supply.sup_id}'
+            )
+        )
+
+    try:
+        bot.answer_callback_query(call.id, f'Загружено {len(supplies_markup.keyboard)} поставок')
+    except telebot.apihelper.ApiTelegramException:
+        pass
+
+    supplies_markup.add(
+        InlineKeyboardButton(
+            text='Показать больше поставок',
+            callback_data=f'more_supplies'
+        )
+    )
+    bot.send_message(
+        chat_id=call.message.chat.id,
+        text='Текущие незакрытые поставки',
+        reply_markup=supplies_markup
     )
 
 
 def compile_orders_to_one_message(orders: list[Order]) -> str:
+    """Собирает все артикулы из заказов и компилирует их в одно сообщение"""
     if orders:
+        articles = [order.article for order in orders]
         message_text = '\n'.join(
-            sorted(
-                [
-                    order.article
-                    for order in orders
-                ]
-            )
+            [
+                f'{article} - {count}шт.'
+                for article, count in Counter(articles.sort()).items()
+            ]
         )
     else:
         message_text = 'В данной поставе нет заказов'
