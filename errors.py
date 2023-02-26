a74a3a5a98a597707ba21a53252143458dd72018
nstonic <118763806+nstonic@users.noreply.github.com>
2023-02-26 14:29:56 +0400

    Fix error checking

--- errors.py
+++ errors.py
@@ -7,22 +7,26 @@ from requests.exceptions import ChunkedEncodingError
 class WBAPIError(HTTPError):
     """Исключения для обработки ошибок запроса к API"""
 
-    def __init__(self, code: str, message: str):
+    def __init__(self, message: str, code: str = None):
         self.message = message
         self.code = code
 
     def __str__(self):
-        return f'{self.code}: {self.message}'
+        return f'{self.code}: {self.message}' if self.code else self.message
 
 
 def check_response(response: Response):
     """Функция для обработки ошибок запросов к API"""
     response.raise_for_status()
-    response_json = response.json()  # может возникнуть requests.exceptions.JSONDecodeError
+    response_json = response.json()
     if response_json.keys() == ('code', 'message'):
         raise WBAPIError(
             code=response_json['code'],
             message=response_json['message']
+        )
+    if response_json.get('error'):
+        raise WBAPIError(
+            message=f'{response_json["errorText"]}: {response_json["additionalErrors"]}'
         )
 
 
